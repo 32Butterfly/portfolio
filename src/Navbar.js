211826b535c6c45a1294285a9a01206f8cfad566
Evaldas Dmitrišin <81704689+32Butterfly@users.noreply.github.com>
2025-07-14 20:05:51 +0300

    Make a simple navbar + hello world

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+const navItems = [
+  { title: 'Home', url: '/' },
+  { title: 'About me', url: '/aboutme' },
+  { title: 'Projects', url: '/projects' },
+];
+
+function Navbar() {
+  return (
+    <nav className="relative h-12 flex items-center px-4">
+      <div className="absolute left-4">
+        <a className="text-lg font-semibold text-slate-800">Evaldas/32Butterfly</a>
+      </div>
+      <div className="mx-auto flex space-x-4">
+      {navItems.map((item) => (
+        <a 
+          href={item.url} 
+          className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{item.title}
+        </a>
+      ))}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
