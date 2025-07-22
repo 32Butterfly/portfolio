d8f9c80e1121e887a87c88f74ca6ad325b1c0c46
Evaldas Dmitrišin <81704689+32Butterfly@users.noreply.github.com>
2025-07-22 23:45:04 +0300

    Try some animations for landing page (will rework)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
+import LandingCard from './LandingCard';
+
 function Home() {
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-slate-800 mb-4">Welcome to My Portfolio</h1>
-      <p className="text-slate-600">
-        Hi! I'm Evaldas (32Butterfly). This is my homepage.
-      </p>
-    </div>
+    <LandingCard/>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+     extend: {
+      keyframes: {
+        slowPulse: {
+          '0%, 100%': { opacity: '0.2' },
+          '50%': { opacity: '0.4' },
+        },
+        mediumPulse: {
+          '0%, 100%': { opacity: '0.3' },
+          '50%': { opacity: '0.6' },
+        },
+        fastPulse: {
+          '0%, 100%': { opacity: '0.4' },
+          '50%': { opacity: '0.8' },
+        },
+      },
+      animation: {
+        slowPulse: 'slowPulse 6s ease-in-out infinite',
+        mediumPulse: 'mediumPulse 4s ease-in-out infinite',
+        fastPulse: 'fastPulse 2s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
